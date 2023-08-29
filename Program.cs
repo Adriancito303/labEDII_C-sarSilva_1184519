6a287552c2a65feb67d42f192687ec69bbb6ea01
Adriancito303 <70454480+Adriancito303@users.noreply.github.com>
2023-08-28 20:16:40 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace lab1EDII_CésarSilva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int opcion;
+                string ruta;
+                Console.WriteLine("Ingrese opcion a realizar");
+                Console.WriteLine("1. Operaciones a travez de CSV");
+                Console.WriteLine("2. Buscar un dato");
+                Console.WriteLine("3. Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        int count = 0;
+                        Console.WriteLine("Inserte ruta absoluta del archivo");
+                        ruta = Console.ReadLine();
+                        string[] archivo = File.ReadAllLines(ruta);
+                        string[,] jason = new string[1000, 1000];
+                        foreach (var linea in archivo)
+                        {
+                            var valores = linea.Split(';');
+                            string operacion = valores[0];
+                            Console.WriteLine(valores[0]);
+                            Console.WriteLine(valores[1]);
+                            Console.ReadLine();
+                            count++;
+
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Que desea buscar");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Adios");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Inserte una opcion valida");
+                Console.ReadLine();
+            }
+        }
+    }
+}
