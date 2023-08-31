a0a8fe10da7ad0837e82104042d41e0cfd43b494
Adriancito303 <70454480+Adriancito303@users.noreply.github.com>
2023-08-31 16:35:46 -0600

    ya inserta, borra y actualiza

--- Program.cs
+++ Program.cs
@@ -50,117 +50,205 @@ namespace lab1EDII_CésarSilva
         public static List<lectura> jtotal = new List<lectura>();
         public static List<serializar> inserte = new List<serializar>();
         public static List<serializar> delete = new List<serializar>();
+        public static List<serializar> patch = new List<serializar>();
         static void Main(string[] args)
         {
             bool volver = true;
             string opi;
             while (volver == true)
             {
-                volver = false;
-                Console.Clear();
-                int opcion;
-                string ruta;
-                Console.WriteLine("Ingrese opcion a realizar");
-                Console.WriteLine("1. Operaciones a travez de CSV");
-                Console.WriteLine("2. Buscar un dato por nombre");
-                Console.WriteLine("3. Buscar un dato por dpi");
-                Console.WriteLine("4. Salir");
-                opcion = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
-                switch (opcion)
+                try
                 {
-                    case 1: //Insertar valores por medio de csv
-                        Console.WriteLine("Inserte ruta absoluta del archivo");
-                        ruta = Console.ReadLine();
-                        string[] archivo = File.ReadAllLines(ruta);
-                        string[] total;
-                        foreach (var linea in archivo)
-                        {
-                            string[] valores = linea.Split(';'); //se divide el csv en 2 partes
-                            lectura guardar = new(valores[0], valores[1]); //se llama a la lista guardar con los valores separados
-                            jtotal.Add(guardar);//se agrega a la lista jtotal los valores guardados
-                            //string cosa;
-                            //cosa = JsonConvert.SerializeObject(lectura);
-                            
-                            guardar.jsond = "[" + guardar.jsond + "]";//se compone el formato json que está en guardar
-                            guardar.jsond = guardar.jsond.Replace("\"\"", "\"");
-                            if (guardar.operacion == "INSERT")
+                    volver = false;
+                    Console.Clear();
+                    int opcion;
+                    string ruta;
+                    Console.WriteLine("----------------------------MENÚ bY CÉSAR SILVA / 1184519----------------------------");
+                    Console.WriteLine("------------------------------Ingrese opcion a realizar------------------------------");
+                    Console.WriteLine("1. Operaciones a travez de un nuevo CSV");
+                    Console.WriteLine("2. Agregar mas operaciones a travez de un CSV");
+                    Console.WriteLine("3. Buscar un dato por nombre");
+                    Console.WriteLine("4. Salir");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    switch (opcion)
+                    {
+                        case 1: //Insertar valores por medio de csv
+                            inserte.Clear();
+                            Console.WriteLine("Inserte ruta absoluta del archivo");
+                            ruta = Console.ReadLine();
+                            string[] archivo = File.ReadAllLines(ruta);
+                            foreach (var linea in archivo)
                             {
-                                personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
-                                inserte.AddRange(personas);//se agregan los valores a la lista inserte
-                            }
-                            else if (guardar.operacion == "DELETE")
-                            {
-                                personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
-                                //inserte.RemoveRange(personas);
-                                delete.AddRange(personas);
+                                string[] valores = linea.Split(';'); //se divide el csv en 2 partes
+                                lectura guardar = new(valores[0], valores[1]); //se llama a la lista guardar con los valores separados
+                                jtotal.Add(guardar);//se agrega a la lista jtotal los valores guardados
+                                                    //string cosa;
+                                                    //cosa = JsonConvert.SerializeObject(lectura);
 
-                                foreach (var item in delete)
+                                guardar.jsond = "[" + guardar.jsond + "]";//se compone el formato json que está en guardar
+                                guardar.jsond = guardar.jsond.Replace("\"\"", "\"");
+                                if (guardar.operacion == "INSERT")
                                 {
-                                    inserte.RemoveAll(inserte =>
-                                                        inserte.name == item.name &&
-                                                        inserte.dpi == item.dpi &&
-                                                        inserte.address == item.address &&
-                                                        inserte.datebirth == item.datebirth);
+                                    personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
+                                    inserte.AddRange(personas);//se agregan los valores a la lista inserte
+                                }
+                                else if (guardar.operacion == "DELETE")
+                                {
+                                    personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
+                                                                                                              //inserte.RemoveRange(personas);
+                                    delete.AddRange(personas);
+
+                                    foreach (var item in delete)
+                                    {
+                                        inserte.RemoveAll(inserte =>
+                                                            inserte.name == item.name &&
+                                                            inserte.dpi == item.dpi &&
+                                                            inserte.address == item.address &&
+                                                            inserte.datebirth == item.datebirth);
+                                    }
+                                    delete.Clear();
+                                }
+                                else if (guardar.operacion == "PATCH")
+                                {
+                                    personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
+                                    patch.AddRange(personas);
+                                    foreach (var item in patch)
+                                    {
+                                        var reemplazo = inserte.Find(inserte =>
+                                                                                inserte.name == item.name &&
+                                                                                inserte.dpi == item.dpi);
+                                        if (reemplazo != null)
+                                        {
+                                            reemplazo.datebirth = item.datebirth;
+                                            reemplazo.address = item.address;
+                                        }
+                                    }
+                                    patch.Clear();
                                 }
                             }
-                        }
-                        Console.Clear();
-                        foreach (var item in inserte)
-                        {
-                            Console.WriteLine(item.name);
-                            Console.WriteLine(item.dpi);
-                            Console.WriteLine(item.datebirth);
-                            Console.WriteLine(item.address);
-                        }
-                        Console.WriteLine("Lectura exitosa");
-                        Console.WriteLine("Volviendo al menú principal");
-                        volver = true;
-                        Console.ReadLine();
-                        break;
-                    case 2:
-                        Console.WriteLine("Que desea buscar");
-                        string busquedan = Console.ReadLine();
-                        Console.WriteLine("Desea volver al menú principal?");
-                        Console.WriteLine("Presione 1 para volver, u otra tecla para salir.");
-                        opi = Console.ReadLine();
-                        if (opi == "1")
-                        {
+                            Console.Clear();
+                            //foreach (var item in inserte)
+                            //{
+                            //    Console.WriteLine(item.name);
+                            //    Console.WriteLine(item.dpi);
+                            //    Console.WriteLine(item.datebirth);
+                            //    Console.WriteLine(item.address);
+                            //}
+                            Console.WriteLine("----------------------------LISTO----------------------------");
+                            Console.WriteLine("----------------------Lectura exitosa------------------------");
+                            Console.WriteLine("-------------operaciones realizada correctamente-------------");
+                            Console.WriteLine("-----------------Volviendo al menú principal-----------------");
                             volver = true;
-                            Console.WriteLine("Volviendo al menú principal");
                             Console.ReadLine();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Adios");
-                            Console.ReadLine();
-                        }
-                        break;
-                    case 3:
-                        Console.WriteLine("Que desea buscar");
-                        string busquedad = Console.ReadLine();
-                        Console.WriteLine("Desea volver al menú principal?");
-                        Console.WriteLine("Presione 1 para volver, u otra tecla para salir.");
-                        opi = Console.ReadLine();
-                        if (opi == "1")
-                        {
+                            break;
+                        case 2:
+                            Console.WriteLine("Inserte ruta absoluta del archivo");
+                            ruta = Console.ReadLine();
+                            string[] archivo2 = File.ReadAllLines(ruta);
+                            foreach (var linea in archivo2)
+                            {
+                                string[] valores = linea.Split(';'); //se divide el csv en 2 partes
+                                lectura guardar = new(valores[0], valores[1]); //se llama a la lista guardar con los valores separados
+                                jtotal.Add(guardar);//se agrega a la lista jtotal los valores guardados
+                                                    //string cosa;
+                                                    //cosa = JsonConvert.SerializeObject(lectura);
+
+                                guardar.jsond = "[" + guardar.jsond + "]";//se compone el formato json que está en guardar
+                                guardar.jsond = guardar.jsond.Replace("\"\"", "\"");
+                                if (guardar.operacion == "INSERT")
+                                {
+                                    personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
+                                    inserte.AddRange(personas);//se agregan los valores a la lista inserte
+                                }
+                                else if (guardar.operacion == "DELETE")
+                                {
+                                    personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
+                                                                                                              //inserte.RemoveRange(personas);
+                                    delete.AddRange(personas);
+
+                                    foreach (var item in delete)
+                                    {
+                                        inserte.RemoveAll(inserte =>
+                                                            inserte.name == item.name &&
+                                                            inserte.dpi == item.dpi &&
+                                                            inserte.address == item.address &&
+                                                            inserte.datebirth == item.datebirth);
+                                    }
+                                    delete.Clear();
+                                }
+                                else if (guardar.operacion == "PATCH")
+                                {
+                                    personas = JsonConvert.DeserializeObject<List<serializar>>(guardar.jsond);//se deserializa el json
+                                    patch.AddRange(personas);
+                                    foreach (var item in patch)
+                                    {
+                                        var reemplazo = inserte.Find(inserte =>
+                                                                                inserte.name == item.name &&
+                                                                                inserte.dpi == item.dpi);
+                                        if (reemplazo != null)
+                                        {
+                                            reemplazo.datebirth = item.datebirth;
+                                            reemplazo.address = item.address;
+                                        }
+                                    }
+                                    patch.Clear();
+                                }
+                            }
+                            Console.Clear();
+                            //foreach (var item in inserte)
+                            //{
+                            //    Console.WriteLine(item.name);
+                            //    Console.WriteLine(item.dpi);
+                            //    Console.WriteLine(item.datebirth);
+                            //    Console.WriteLine(item.address);
+                            //}
+                            Console.WriteLine("----------------------------LISTO----------------------------");
+                            Console.WriteLine("----------------------Lectura exitosa------------------------");
+                            Console.WriteLine("-------------operaciones realizada correctamente-------------");
+                            Console.WriteLine("-----------------Volviendo al menú principal-----------------");
                             volver = true;
-                            Console.WriteLine("Volviendo al menú principal");
                             Console.ReadLine();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Adios");
+                            break;
+                        case 3:
+                            Console.WriteLine("----------------------Que desea buscar----------------------");
+                            string busquedan = Console.ReadLine();
+                            Console.WriteLine("Desea volver al menú principal?");
+                            Console.WriteLine("Presione 1 para volver, u otra tecla para salir.");
+                            opi = Console.ReadLine();
+                            if (opi == "1")
+                            {
+                                volver = true;
+                                Console.Clear();
+                                Console.WriteLine("-----------------Volviendo al menú principal-----------------");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("----------------------------Adios----------------------------");
+                                Console.ReadLine();
+                            }
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("----------------------------Adios----------------------------");
                             Console.ReadLine();
-                        }
-                        break;
-                    case 4:
-                        Console.WriteLine("Adios");
-                        Console.ReadLine();
-                        break;
-                    default:
-                        Console.WriteLine("Opcion invalida");
-                        break;
+                            break;
+                        default:
+                            Console.WriteLine("----------------------------Opcion invalida----------------------------");
+                            Console.ReadLine();
+                            volver = true;
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    volver = true;
+                    Console.WriteLine("---------------------------ERROR----------------------------");
+                    Console.WriteLine("------------------------favor revisar-----------------------");
+                    Console.ReadLine();
                 }
             }
         }
